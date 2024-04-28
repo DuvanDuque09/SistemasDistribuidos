4b5eb029c215abb73724a3cb4e8cab8f7c10b089
AlejandroP26 <apalencia-2020b@corhuila.edu.co>
2024-04-27 23:13:02 -0500

    Finilización visual HOME

--- Project/resources/views/livewire/dashboard/admin.blade.php
+++ Project/resources/views/livewire/dashboard/admin.blade.php
@@ -1,4 +1,4 @@
-<div class="h-screen w-full p-4 overflow-y-auto md:pb-4 pb-36">
+<div class="h-screen w-full p-2 overflow-y-auto md:pb-4 pb-36">
     <div class="w-full px-6 mx-auto">
         <div class="mt-3 flex items-center">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNVtB5xaYM_R5sVotcaEuG-4thCAuwQo-PKn_iFpBmnA&s"
@@ -10,7 +10,7 @@
 
         <br>
         <div class="flex">
-            <div class="relative items-center p-0 mt-8 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/3 mr-4">
+            <div class="relative items-center p-0 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/3 mr-4">
                 <span
                     class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
                 <div class="grid grid-cols-1 gap-2 p-2 mt-2 relative">
@@ -24,18 +24,17 @@
                 </div>
             </div>
 
-            <div class="relative items-center p-0 mt-8 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/2">
+            <div class="relative items-center p-0 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/2">
                 <span
                     class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
                 <div class="grid grid-cols-1 gap-2 p-2 mt-2 relative">
-                    <canvas id="myChart" style="width: 100%; height: 100%;"></canvas>
+                    <canvas id="myChart" style="width: 100%; height: 90%;"></canvas>
                 </div>
             </div>
-
         </div>
 
-        <div class="w-full rounded-2xl border-gray-400 flex gap-4 mt-4">
-            <div class="relative flex items-center p-0 mt-8 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
+        <div class="w-full rounded-2xl border-gray-400 flex gap-4">
+            <div class="relative flex items-center p-0 mt-6 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
                 <span
                     class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full">
                     <div class="grid grid-cols-2 gap-2 p-2 mt-2">
@@ -70,7 +69,7 @@
                         <img src="https://c0.klipartz.com/pngpicture/756/62/gratis-png-iconos-de-la-computadora-llamada-telefonica-iphone-iphone.png"
                             class="h-10/12 w-10 rounded-2xl">
                         <div class="ml-2">
-                            <p><b>Llamada recibida</b></p>
+                            <p><b>Llamada Perdida</b></p>
                             <label for="" value="" class="opacity-60">5:12 . Colombia</label>
                         </div>
                         <label for="" value="" class="ml-auto"><b>0 min</b></label>
@@ -79,7 +78,7 @@
                 </span>
             </div>
 
-            <div class="relative flex items-center p-0 mt-8 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
+            <div class="relative flex items-center p-0 mt-6 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
                 <span
                     class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full">
                     <div class="grid grid-cols-2 gap-2 p-2 mt-2">
@@ -94,6 +93,31 @@
                 </span>
             </div>
         </div>
+
+        <div
+            class="relative items-center p-0 mt-2 overflow-hidden bg-center bg-cover h-21 rounded-2xl w-1/3 mr-4 ml-auto">
+            <span
+                class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
+            <div class="flex flex-col p-4 relative">
+                <div class="flex">
+                    <label class="opacity-50 p-2 mr-4"> % CONTESTADAS </label>
+                    <label class="opacity-50 p-2 mb-4"> % PERDIDAS </label>
+                </div>
+                <div class="flex">
+                    <!-- Barra de progreso para % CONTESTADAS -->
+                    <div class="relative h-4 rounded-full overflow-hidden bg-green-200 w-40 flex items-center mr-4">
+                        <div class="absolute top-0 left-0 h-full bg-green-500" style="width: 25%;"></div>
+                        <div class="text-center w-full">30%</div>
+                    </div>
+                    <!-- Barra de progreso para % PERDIDAS -->
+                    <div class="relative h-4 rounded-full overflow-hidden bg-yellow-200 w-40 flex items-center mb-4">
+                        <div class="absolute top-0 right-0 h-full bg-yellow-500" style="width: 24%;"></div>
+                        <div class="text-center w-full">30%</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 
@@ -113,7 +137,7 @@
             type: 'bar',
             data: {
                 labels: daysOfWeek.slice(0, today +
-                1), // Cortar los días de la semana hasta el día actual
+                    1), // Cortar los días de la semana hasta el día actual
                 datasets: [{
                     label: '# de Llamadas por Semana',
                     data: data,
