c68f947aababf99ffa04cfc938412ad84db28fff
AlejandroP26 <apalencia-2020b@corhuila.edu.co>
2024-04-29 18:40:55 -0500

    Funcionalidad Home

--- Project/app/Livewire/Dashboard/Admin.php
+++ Project/app/Livewire/Dashboard/Admin.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Dashboard;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
+
 use Livewire\Component;
 
 class Admin extends Component
 {
+    public $user;
+    public $fechaActual;
+
     public function render()
     {
+        // Establecer la localización en español
+        Carbon::setLocale('es');
+
+        // Asignar el valor de $user dentro del método render()
+        $this->user = Auth::user();
+
+        // Obtener la fecha actual y almacenarla en la variable $fechaActual
+        $this->fechaActual = Carbon::now()->format('l, j \d\e F \d\e Y');
+
         return view('livewire.dashboard.admin')
             ->layout('layouts.app');
     }
--- Project/resources/views/livewire/dashboard/admin.blade.php
+++ Project/resources/views/livewire/dashboard/admin.blade.php
@@ -1,126 +1,116 @@
-<div class="h-screen w-full p-2 overflow-y-auto md:pb-4 pb-36">
-    <div class="w-full px-6 mx-auto">
-        <div class="mt-3 flex items-center">
+{{--  <div class="h-screen">  --}}
+<div
+    class="grid md:grid-cols-2 grid-cols-1 h-screen w-full p-4 md:overflow-hidden overflow-y-auto md:pb-4 pb-36 relative">
+    <div class="absolute top-3 left-3">
+        <div class="flex items-center">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNVtB5xaYM_R5sVotcaEuG-4thCAuwQo-PKn_iFpBmnA&s"
                 alt="" class="h-10/12 w-10 rounded-2xl mr-3">
-            <label for="" value="" class=""><b>Bienvenido</b>, Admin</label>
+            <label for="" value="" class=""><b>Bienvenido</b>, {{ $user->name }}</label>
             <img src="https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/share/1f44b-1f3fb.jpg"
                 alt="" class="h-10/12 w-10 rounded-2xl">
         </div>
-
-        <br>
-        <div class="flex">
-            <div class="relative items-center p-0 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/3 mr-4">
-                <span
-                    class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
-                <div class="grid grid-cols-1 gap-2 p-2 mt-2 relative">
-                    <div style="z-index: 1;">
-                        <h2 class="font-bold border-gray-400 p-2 text-2xl mt-5">Llamadas recibidas</h2>
-                        <label for="" value="" class="opacity-50 p-2">MIÉRCOLES 24 ABRIL 2024</label>
-                    </div>
-                    <div class="grid grid-cols-1 gap-2 p-4 bg-blue-100 absolute top-6 right-14 z-10">
-                        <p style="font-size: 5vh;"><b>200</b></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="relative items-center p-0 overflow-hidden bg-center bg-cover h-40 rounded-2xl w-1/2">
-                <span
-                    class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
-                <div class="grid grid-cols-1 gap-2 p-2 mt-2 relative">
-                    <canvas id="myChart" style="width: 100%; height: 90%;"></canvas>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full rounded-2xl border-gray-400 flex gap-4">
-            <div class="relative flex items-center p-0 mt-6 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
-                <span
-                    class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full">
-                    <div class="grid grid-cols-2 gap-2 p-2 mt-2">
-                        <h2 class="text-xl"><b>Llamadas recibidas</b></h2>
-                    </div>
-                    <hr class="w-full border-b-2 border-gray-400">
-
-                    <br>
-                    <div class="ml-4 mr-4 flex items-center">
-                        <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
-                            class="h-10/12 w-10 rounded-2xl">
-                        <div class="ml-2">
-                            <p><b>Llamada recibida</b></p>
-                            <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                        </div>
-                        <label for="" value="" class="ml-auto"><b>10 min</b></label>
-                    </div>
-
-                    <br>
-                    <div class="ml-4 mr-4 flex items-center">
-                        <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
-                            class="h-10/12 w-10 rounded-2xl">
-                        <div class="ml-2">
-                            <p><b>Llamada recibida</b></p>
-                            <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                        </div>
-                        <label for="" value="" class="ml-auto"><b>10 min</b></label>
-                    </div>
-
-                    <br>
-                    <div class="ml-4 mr-4 flex items-center">
-                        <img src="https://c0.klipartz.com/pngpicture/756/62/gratis-png-iconos-de-la-computadora-llamada-telefonica-iphone-iphone.png"
-                            class="h-10/12 w-10 rounded-2xl">
-                        <div class="ml-2">
-                            <p><b>Llamada Perdida</b></p>
-                            <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                        </div>
-                        <label for="" value="" class="ml-auto"><b>0 min</b></label>
-                    </div>
-
-                </span>
-            </div>
-
-            <div class="relative flex items-center p-0 mt-6 overflow-hidden bg-center bg-cover h-80 rounded-2xl w-1/2">
-                <span
-                    class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full">
-                    <div class="grid grid-cols-2 gap-2 p-2 mt-2">
-                        <h2 class="text-xl"><b>Resultados</b></h2>
-                    </div>
-                    <hr class="w-full border-b-2 border-gray-400 pl-5">
-                    <br>
-
-                    <div>
-
-                    </div>
-                </span>
-            </div>
-        </div>
-
-        <div
-            class="relative items-center p-0 mt-2 overflow-hidden bg-center bg-cover h-21 rounded-2xl w-1/3 mr-4 ml-auto">
-            <span
-                class="absolute inset-0 bg-center bg-cover bg-gradient-to-tl from-gray-50 to-gray-300 opacity-70 rounded-3 border-3 h-full"></span>
-            <div class="flex flex-col p-4 relative">
-                <div class="flex">
-                    <label class="opacity-50 p-2 mr-4"> % CONTESTADAS </label>
-                    <label class="opacity-50 p-2 mb-4"> % PERDIDAS </label>
-                </div>
-                <div class="flex">
-                    <!-- Barra de progreso para % CONTESTADAS -->
-                    <div class="relative h-4 rounded-full overflow-hidden bg-green-200 w-40 flex items-center mr-4">
-                        <div class="absolute top-0 left-0 h-full bg-green-500" style="width: 25%;"></div>
-                        <div class="text-center w-full">30%</div>
-                    </div>
-                    <!-- Barra de progreso para % PERDIDAS -->
-                    <div class="relative h-4 rounded-full overflow-hidden bg-yellow-200 w-40 flex items-center mb-4">
-                        <div class="absolute top-0 right-0 h-full bg-yellow-500" style="width: 24%;"></div>
-                        <div class="text-center w-full">30%</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
-</div>
+    {{--  <div class="w-full h-full">  --}}
+    <div class="flex flex-col md:h-[98%] w-[99%] p-4 gap-4 md:relative mt-16">
+        <div class="grid grid-cols-2 md:absolute top-4 md:w-[97%] md:h-[20%] gap-4">
+            <div class="bg-white w-full h-full py-6 px-4 rounded-lg shadow">
+                <h2 class="text-1x2 font-extrabold p-2 text-gray-900">Llamadas recibidas</h2>
+                <label for="" value="" class="opacity-50 p-2 text-2">{{ $fechaActual }}</label>
+                <div class="grid grid-cols-1 gap-1 p-2 bg-blue-100 w-[24%] z-10 rounded-3"
+                    style="margin-top:5%; margin-left:34%;">
+                    <p style="font-size: 4vh;top:5%;"><b>200</b></p>
+                </div>
+            </div>
 
+            <div class="bg-white w-full h-full py-6 px-2 rounded-lg shadow">
+                <div class="mt-4">
+                    <div class="flex items-center mb-4">
+                        <p class="flex-1 opacity-50 p-2 mr-4 text-2">%_CONTESTADAS</p>
+                        <p class="flex-1 opacity-50 p-2 mr-4 text-2">%_PERDIDAS</p>
+                    </div>
+
+                    <div class="grid grid-cols-2 gap-1 p-2 rounded-3 w-full">
+                        <!-- Barra de progreso para % CONTESTADAS -->
+                        <div class="relative h-4 w-full rounded-full overflow-hidden bg-green-200 flex items-center">
+                            <div class="absolute top-0 left-0 bg-green-500" style="width: 30%; height: 100%;"></div>
+                            <div class="text-center w-full">30%</div>
+                        </div>
+                        <!-- Barra de progreso para % PERDIDAS -->
+                        <div class="relative h-4 w-full rounded-full overflow-hidden bg-yellow-200 flex items-center">
+                            <div class="absolute top-0 right-0 bg-yellow-500" style="width: 30%; height: 100%;"></div>
+                            <div class="text-center w-full">30%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="md:w-[90%] md:h-[55%] px-4 md:absolute bottom-0 ">
+        <div class="bg-white md:w-[50%] md:h-[95%] px-4 rounded-lg shadow md:absolute">
+            <div class="grid grid-cols-2 gap-2 p-2 mt-2">
+                <h2 class="text-xl"><b>Llamadas recibidas</b></h2>
+            </div>
+            <hr class="w-full border-b-2 border-gray-200">
+
+            <br>
+            <div class="ml-4 mr-4 flex items-center">
+                <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
+                    class="h-10/12 w-10 rounded-2xl">
+                <div class="ml-2">
+                    <p><b>Llamada recibida</b></p>
+                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
+                </div>
+                <label for="" value="" class="ml-auto"><b>10 min</b></label>
+            </div>
+
+            <br>
+            <div class="ml-4 mr-4 flex items-center">
+                <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
+                    class="h-10/12 w-10 rounded-2xl">
+                <div class="ml-2">
+                    <p><b>Llamada recibida</b></p>
+                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
+                </div>
+                <label for="" value="" class="ml-auto"><b>10 min</b></label>
+            </div>
+
+            <br>
+            <div class="ml-4 mr-4 flex items-center">
+                <img src="https://c0.klipartz.com/pngpicture/756/62/gratis-png-iconos-de-la-computadora-llamada-telefonica-iphone-iphone.png"
+                    class="h-10/12 w-10 rounded-2xl">
+                <div class="ml-2">
+                    <p><b>Llamada Perdida</b></p>
+                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
+                </div>
+                <label for="" value="" class="ml-auto"><b>0 min</b></label>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="flex">
+        <div class="md:w-[90%] md:h-[55%] px-4 md:absolute bottom-0 ">
+            <div class="bg-white md:w-[50%] md:h-[95%] px-4 rounded-lg shadow md:absolute">
+                <div class="grid grid-cols-2 gap-2 p-2 mt-2">
+                    <h2 class="text-xl"><b>Resultados</b></h2>
+                </div>
+                <hr class="w-full border-b-2 border-gray-200 pl-5">
+            </div>
+        </div>
+
+        <div class="md:w-[90%] md:h-[55%] px-4" style="margin-top: 5px;">
+            <div class="bg-white md:w-[100%] md:h-[52%] py-4 px-3 rounded-lg shadow" style="margin-top:14%;">
+                <div>
+                    <canvas id="myChart" style="width: 100%; height: 100%;"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
