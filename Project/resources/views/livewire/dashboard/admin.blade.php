17bff1208c1a230c777b0ed3914d0df852d991b7
AlejandroP26 <apalencia-2020b@corhuila.edu.co>
2024-05-06 21:32:00 -0500

    Cambio del Home

--- Project/resources/views/livewire/dashboard/admin.blade.php
+++ Project/resources/views/livewire/dashboard/admin.blade.php
@@ -1,24 +1,27 @@
 {{--  <div class="h-screen">  --}}
 <div
     class="grid md:grid-cols-2 grid-cols-1 h-screen w-full p-4 md:overflow-hidden overflow-y-auto md:pb-4 pb-36 relative">
+
     <div class="absolute top-3 left-3">
         <div class="flex items-center">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNVtB5xaYM_R5sVotcaEuG-4thCAuwQo-PKn_iFpBmnA&s"
                 alt="" class="h-10/12 w-10 rounded-2xl mr-3">
-            <label for="" value="" class=""><b>Bienvenido</b>, {{ $user->name }}</label>
+            <label for="" value="" class=""><b>Bienvenido</b>, {{ Auth::user()->name }}</label>
             <img src="https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/share/1f44b-1f3fb.jpg"
                 alt="" class="h-10/12 w-10 rounded-2xl">
         </div>
     </div>
     {{--  <div class="w-full h-full">  --}}
+
+
     <div class="flex flex-col md:h-[98%] w-[99%] p-4 gap-4 md:relative mt-16">
         <div class="grid grid-cols-2 md:absolute top-4 md:w-[97%] md:h-[20%] gap-4">
             <div class="bg-white w-full h-full py-6 px-4 rounded-lg shadow">
                 <h2 class="text-1x2 font-extrabold p-2 text-gray-900">Llamadas recibidas</h2>
-                <label for="" value="" class="opacity-50 p-2 text-2">{{ $fechaActual }}</label>
+                <label for="" value="" class="opacity-50 p-2 text-2">{{ \Carbon\Carbon::parse($fechaActual)->setTimezone('America/Bogota')->format('l, j \d\e F \d\e Y') }}</label>
                 <div class="grid grid-cols-1 gap-1 p-2 bg-blue-100 w-[24%] z-10 rounded-3"
                     style="margin-top:5%; margin-left:34%;">
-                    <p style="font-size: 4vh;top:5%;"><b>200</b></p>
+                    <p style="font-size: 4vh;top:5%; text-align:center;"><b>{{ count($datosDelDiaActual) }}</b></p>
                 </div>
             </div>
 
@@ -46,46 +49,30 @@
         </div>
     </div>
 
-    <div class="md:w-[90%] md:h-[55%] px-4 md:absolute bottom-0 ">
-        <div class="bg-white md:w-[50%] md:h-[95%] px-4 rounded-lg shadow md:absolute">
+    <div class="md:w-[90%] md:h-[55%] px-8 md:absolute bottom-0 ">
+        <div class="bg-white md:w-[52%] md:h-[95%] px-4 rounded-lg shadow md:absolute">
             <div class="grid grid-cols-2 gap-2 p-2 mt-2">
                 <h2 class="text-xl"><b>Llamadas recibidas</b></h2>
             </div>
             <hr class="w-full border-b-2 border-gray-200">
-
             <br>
-            <div class="ml-4 mr-4 flex items-center">
-                <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
-                    class="h-10/12 w-10 rounded-2xl">
-                <div class="ml-2">
-                    <p><b>Llamada recibida</b></p>
-                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                </div>
-                <label for="" value="" class="ml-auto"><b>10 min</b></label>
+            <div class="ml-4 mr-4">
+                @foreach ($datosDelDiaActual as $dato)
+                    <div class="flex items-center mb-4">
+                        <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
+                            class="h-10/12 w-10 rounded-2xl">
+                        <div class="ml-2">
+                            <p><b>Llamada recibida</b></p>
+                            <label for="" value=""
+                                class="opacity-60">{{ Carbon\Carbon::parse($dato->start_date)->setTimezone('America/Bogota')->format('H:i') }} .
+                                {{ $dato->phone }}</label>
+                        </div>
+                        <label for="" value=""
+                            class="ml-auto"><b>{{ round(Carbon\Carbon::parse($dato->start_date)->diffInMinutes(Carbon\Carbon::parse($dato->end_date)), 1) }}
+                                min</b></label>
+                    </div>
+                @endforeach
             </div>
-
-            <br>
-            <div class="ml-4 mr-4 flex items-center">
-                <img src="https://c0.klipartz.com/pngpicture/759/922/gratis-png-logo-del-telefono-iphone-telefono-smartphone-telefono-thumbnail.png"
-                    class="h-10/12 w-10 rounded-2xl">
-                <div class="ml-2">
-                    <p><b>Llamada recibida</b></p>
-                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                </div>
-                <label for="" value="" class="ml-auto"><b>10 min</b></label>
-            </div>
-
-            <br>
-            <div class="ml-4 mr-4 flex items-center">
-                <img src="https://c0.klipartz.com/pngpicture/756/62/gratis-png-iconos-de-la-computadora-llamada-telefonica-iphone-iphone.png"
-                    class="h-10/12 w-10 rounded-2xl">
-                <div class="ml-2">
-                    <p><b>Llamada Perdida</b></p>
-                    <label for="" value="" class="opacity-60">5:12 . Colombia</label>
-                </div>
-                <label for="" value="" class="ml-auto"><b>0 min</b></label>
-            </div>
-
         </div>
     </div>
 
@@ -93,9 +80,10 @@
         <div class="md:w-[90%] md:h-[55%] px-4 md:absolute bottom-0 ">
             <div class="bg-white md:w-[50%] md:h-[95%] px-4 rounded-lg shadow md:absolute">
                 <div class="grid grid-cols-2 gap-2 p-2 mt-2">
-                    <h2 class="text-xl"><b>Resultados</b></h2>
+                    <h2 class="text-xl"><b>Pico de Llamadas </b></h2>
                 </div>
                 <hr class="w-full border-b-2 border-gray-200 pl-5">
+                <canvas id="picoLlamada"></canvas>
             </div>
         </div>
 
@@ -107,27 +95,27 @@
             </div>
         </div>
     </div>
-
-
-
 </div>
+
+
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var today = new Date().getDay(); // Obtener el día actual (0 para Domingo, 1 para Lunes, etc.)
-        var daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var data = [12, 19, 3, 5, 2, 3, 8].slice(0, today + 1); // Cortar los datos hasta el día actual
+        // Ajustar el día actual para que corresponda al índice correcto en el array
+        today = (today === 0) ? 6 : today - 1;
+        var daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+        var ctx1 = document.getElementById('myChart').getContext('2d');
+        var llamadasPorDia = {!! json_encode($llamadasPorDiaArray) !!};
+        var data = Object.values(llamadasPorDia); // No es necesario cortar los datos
         var backgroundColors = Array(data.length).fill('rgba(178, 172, 172)'); // Colores predeterminados
-
         // Cambiar el color del día actual a azul
         backgroundColors[today] = 'rgba(54, 162, 235)';
 
-        var myChart = new Chart(ctx, {
+        var myChart1 = new Chart(ctx1, {
             type: 'bar',
             data: {
-                labels: daysOfWeek.slice(0, today +
-                    1), // Cortar los días de la semana hasta el día actual
+                labels: daysOfWeek, // No es necesario cortar los días de la semana
                 datasets: [{
                     label: '# de Llamadas por Semana',
                     data: data,
@@ -153,5 +141,35 @@
                 }
             }
         });
+    });
+
+    // Datos proporcionados desde el controlador PHP
+    var dataLlamadas = {!! json_encode($horaPico) !!};
+    var horas = Object.keys(dataLlamadas['Hora']);
+    var totales = Object.values(dataLlamadas['TotalHora']);
+
+    // Configurar la gráfica
+    var ctx = document.getElementById('picoLlamada').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar', // Tipo de gráfica: barras horizontales
+        data: {
+            labels: horas, // Horas en el eje X
+            datasets: [{
+                label: 'Total de Llamadas', // Etiqueta de la serie
+                data: totales, // Totales en el eje Y
+                backgroundColor: 'rgba(54, 162, 235, 0.8)', // Color de las barras
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: true // Empezar el eje Y desde cero
+                    }
+                }]
+            }
+        }
     });
 </script>
